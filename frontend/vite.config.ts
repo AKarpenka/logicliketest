c8639a32f72de928b9bc1cd7c30e3cbb44dc378e
Anastasiya <knesty@yandex.by>
2025-10-04 23:35:53 +0300

    fix: deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,25 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          antd: ['antd', '@ant-design/icons'],
-        },
-        assetFileNames: (assetInfo) => {
-          const extType = assetInfo.name.split('.')[1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return 'assets/images/[name]-[hash][extname]'
-          }
-          if (/css/i.test(extType)) {
-            return 'assets/[name]-[hash][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
-        },
-      },
-    },
+    cssCodeSplit: false,
   },
   server: {
     port: 5173,
